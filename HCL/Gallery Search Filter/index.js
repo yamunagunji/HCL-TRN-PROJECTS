91a60d92a7a5fb15d8b027770fccba7e41288a1b
yamunagunji <yamunagunji@gmail.com>
2021-06-25 17:43:16 +0530

    Count Of words In a string

---
+++ HCL/Gallery Search Filter/index.js
@@ -0,0 +1,80 @@
+function searchImages(a){
+  //console.log('test');
+  var watches = document.getElementsByClassName("watches").value;
+  var cars = document.querySelectorAll("car").value;
+  var bikes = document.querySelectorAll("bike").value;
+  console.log(watches);
+}
+
+/*console.log("yamuna");
+//searchImages(a = watches);
+//searchImages(a = cars);
+//searchImages(a = bikes);
+
+
+function searchImages(a)
+{
+    for (i=0; i<4; i++){
+     var watches = document.getElementsByClassName(".watches");
+      return watches[i];
+    }
+    
+    for(j=0;j<4;j++){
+     var cars = document.querySelectorAll(".car");
+      return cars[j];
+    }
+    
+    for(k=0;k<4;k++)
+    {
+        var bikes = document.querySelectorAll(".bike");
+        return bikes[k];
+    }
+    
+
+}
+  searchImages( watches);
+  searchImages( cars);
+  searchImages( bikes);
+  
+  function searchImages(a){
+    var a;
+    //var watches = document.getElementsByClassName(".watches").value;
+    var cars = document.getElementsByClassName(".car").value;
+    //var bikes = document.getElementsByClassName(".bikes").value;
+      switch(cars)
+      {
+        case 1 : "cars"
+        var a = document.getElementsByClassName(".car").value;
+        console.log(a);
+        break;
+
+      }
+    }
+
+    /*function searchImages(a){
+
+     var cars = document.getElementsByClassName(".car");
+     if(a == cars)
+     {
+       return cars;
+     }else
+     {
+       return null;
+     }
+
+    }
+    
+    function searchImages(a){
+
+      var bikes = document.getElementsByClassName(".bikes");
+      if(a == bikes)
+      {
+        return bikes;
+      }else
+      {
+        return null;
+      }
+ 
+     }*/
+ 
+    
